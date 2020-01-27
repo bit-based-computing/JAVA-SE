c5e4a166474403d76ec2a68edd06ec4c1e811c63
foysalahmmed <foysalahmmed73@gmail.com>
2020-01-27 23:40:16 +0600

    Array declaration format

---
+++ src/com/foysal/dataTypes/Arrays.java
@@ -0,0 +1,16 @@
+package com.foysal.dataTypes;
+
+public class Arrays {
+    public static void main(String[] args) {
+        // Array declaration.......
+        int aa[] = new int[10];
+        int[] bb = new int[10];
+        int[] dd, ee, cc = new int[10];
+        int ff[], gg[], hh[] = new int[10];
+
+        // multidimensional  array
+        int aaa[][] = new int[5][5];
+
+        // same as for all datatype
+    }
+}
